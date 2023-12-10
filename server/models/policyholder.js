6b53ff4330a4b3bc1d55495cc338667c073e98d5
scott1991 <cakepbm@gmail.com>
2023-12-11 01:40:42 +0800

    Add model Policyholder

---
+++ server/models/policyholder.js
@@ -0,0 +1,51 @@
+import { DataTypes } from 'sequelize';
+
+export default (sequelize) => {
+  const Policyholder = sequelize.define('Policyholder', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    code: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    registration_date: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    introducer_code: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    depth: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    parent_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    lchild_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    rchild_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    }
+  }, {});
+
+    Policyholder.associate = (models) => {
+      Policyholder.belongsTo(models.Policyholder, { as: 'parent', foreignKey: 'parent_id' });
+      Policyholder.hasOne(models.Policyholder, { as: 'lchild', foreignKey: 'lchild_id' });
+      Policyholder.hasOne(models.Policyholder, { as: 'rchild', foreignKey: 'rchild_id' });
+    };
+
+  return Policyholder;
+};
